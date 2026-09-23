97c154a4754620b14ccf4e6fd6aca5226810edbf
diegoq10 <dieguitoqb947@gmail.com>
2026-09-22 21:17:50 -0500

    mongoose connected

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose  from "mongoose"
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/bookshelfdb")
+.then(() => {
+  console.log("Conectado a MongoDB")
+})
+.catch((err) => {
+  console.log("Error de conexion", err)
+})
+
+
+
 
 app.use(express.json());
 
